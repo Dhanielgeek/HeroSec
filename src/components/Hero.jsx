ea2fcde5c548f91478cff04631ceca433af0c2fe
Dhanielgeek <dhanielknightz46@gmail.com>
2024-10-03 10:32:30 +0100

    testing phase 2

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -5,28 +5,27 @@ import img3 from "../assets/Auto Layout Vertical (3).png";
 import img4 from "../assets/Auto Layout Vertical (4).png";
 import img5 from "../assets/Auto Layout Vertical (5).png";
 import img6 from "../assets/Auto Layout Vertical (6).png";
-import { img } from "framer-motion/client";
 
 const Hero = () => {
   const ImgArry = [img1, img2, img3, img4, img5, img6];
 
   return (
     <div className="Hero_img w-full h-[50rem] bg-[#00000f] flex-col flex justify-center items-center">
-      <div className="w-[60%] h-[60%]  flex justify-center space-y-8 items-center flex-col max-md:w-[90%]">
-        <h2 className=" text-6xl text-center font-bold text-white max-md:text-2xl">
+      <div className="w-[60%] h-[50%]  flex justify-center space-y-8 items-center flex-col max-md:w-[90%]">
+        <h2 className=" text-6xl text-center font-bold text-white max-md:text-5xl">
           Power{" "}
           <span className="bg-gradient-to-r from-[#de53ff] via-[#f3b996] to-[#f9da76] bg-clip-text text-transparent font-bold">
             Automotive AI
           </span>{" "}
           With Your Data
         </h2>
-        <p className=" text-xl text-center text-white font-medium max-md:text-sm">
+        <p className=" text-xl text-center text-slate-200 font-medium max-md:text-sm">
           Make the best models with the best data. Scale Data Engine leverages
           your enterprise data, and with Scale Generative AI Platform, safely
           unlocks the value of AI.
         </p>
-        <div className="w-[60%] h-[10%]  flex justify-center items-center gap-2 ">
-          <button className=" max-md:px-1 flex justify-center items-center px-3 py-2 bg-custom-gradient rounded text-white font-semibold gap-2">
+        <div className="w-[60%] h-[10%]  flex justify-center items-center gap-2 max-md:w-[90%] ">
+          <button className=" flex justify-center items-center px-3 py-3 bg-custom-gradient rounded text-white font-semibold gap-2">
             Book a Demo <FaArrowRight />{" "}
           </button>
           <p className=" flex justify-center items-center gap-2 font-semibold  text-white">
